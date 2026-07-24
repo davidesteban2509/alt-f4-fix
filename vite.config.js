d980f7c78059ec4a64cd8706d84ab32247363cb8
Alt-F4 Fix <admin@altf4fix.com>
2026-07-24 16:27:10 -0500

    Fix base path for Vercel root domain deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alt-f4-fix/',
+  base: process.env.VERCEL ? '/' : './',
   server: {
     port: 3000,
     open: true
